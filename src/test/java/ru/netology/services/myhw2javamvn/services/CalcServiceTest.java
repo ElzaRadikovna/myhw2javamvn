597494f94c7947e47e3c73fbf4d3d6ed7d7bd2ea
Neto test <akmalova.elza@yandex.ru>
2023-11-18 18:18:58 +0300

    Gitignore added

---
+++ src/test/java/ru/netology/services/myhw2javamvn/services/CalcServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.services.myhw2javamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.services.myhw2javamvn.CalcService;
+
+public class CalcServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/calc.csv")
+    public void testVocationMounths(int expected, int income, int expenses, int threshold) {
+        CalcService service = new CalcService();
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(actual, expected);
+    }
+}
